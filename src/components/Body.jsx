259d36da2107f9e81d9e72cd9a084a0d12fd3f47
preetsinghwal <preetsinghwal38@gmail.com>
2026-03-02 15:47:57 +0530

    devTinder-web: redirect to login if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
+import axios from 'axios';
 import Footer from './Footer'
 import Navbar from './NavBar'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
+import { BASE_URL } from '../utils/constants';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 function Body() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const userData = useSelector((store)=> store.user);
+
+    const fetchUser = async () => {
+        if(userData) return
+        
+        try {
+            const res = await axios.get(BASE_URL + '/profile/view', {withCredentials: true});
+            dispatch(addUser(res.data));
+
+        } catch(err) {
+            if(err.status === 401) {
+                navigate('/login');
+            }
+            console.error(err);
+        }
+
+    }
+
+    useEffect(()=> {
+        fetchUser();
+    }, [])
+
     return (
         <div>
             <Navbar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router';
 
 function Navbar() {
     const user = useSelector((store) => store.user);
     return (
         <div className="navbar bg-base-300">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl text-white">DevTinder</a>
+                <Link to="/" className="btn btn-ghost text-xl text-white">DevTinder</Link>
             </div>
             {user && <div className="flex gap-2 items-center">
                 {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
@@ -24,9 +25,9 @@ function Navbar() {
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li>
-                            <a className="justify-between">
+                            <Link to="/profile" className="justify-between">
                                 Profile
-                            </a>
+                            </Link>
                         </li>
                         <li><a>Settings</a></li>
                         <li><a>Logout</a></li>
