7c70ab1c0b94b303d0b5fe0ebcc314af1d59bb93
preetsinghwal <preetsinghwal38@gmail.com>
2026-03-21 13:46:43 +0530

    devTinder-web: added requests page and its reducer, added into store

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -46,6 +46,7 @@ function Navbar() {
                             </Link>
                         </li>
                         <li><Link to="/connections">Connections</Link></li>
+                        <li><Link to="/requests">Requests</Link></li>
                         <li><a onClick={handleLogout}>Logout</a></li>
                     </ul>
                 </div>
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReduer from './userSlice';
 import feedReducer from "./feedSlice";
 import connectionReducer from "./connectionSlice";
+import requestReducer from "./requestSlice";
 
 
 const appStore = configureStore({
     reducer: {
         user: userReduer,
         feed: feedReducer,
-        connections: connectionReducer
+        connections: connectionReducer,
+        requests: requestReducer
     }
 });
 
